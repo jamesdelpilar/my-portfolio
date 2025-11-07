ef81e7744a27e36ab24466c5d17fb698d1d57575
James Del Pilar <71074350+jamesdelpilar@users.noreply.github.com>
2025-11-06 22:47:30 -0500

    Toggle functionality for project gallery implemented (placeholder info)

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
+// Submission form refs
 const nameInput = document.getElementById('name');
 const emailInput = document.getElementById('email');
 const messageInput = document.getElementById('message');
 const form = document.getElementById("contact-form");
 const submitButton = document.getElementById("submit-button");
 
-// Form validation to check if name and message fields are blank
+// Project Gallery refs
+const project1_button = document.getElementById('project1_button');
+const project1_text = document.getElementById('project1-description');
+
+// Form validation to check if name, message and email fields are blank
 form.addEventListener("submit", (event) => {
     let isValid = true;
     submitButton.disabled = false;
@@ -32,3 +37,10 @@ form.addEventListener("submit", (event) => {
       submitButton.disabled = false;
     }
   });
+
+// Project show/hide gallery
+    project1_button.addEventListener('click', () => {
+      const isHidden = project1_text.classList.contains('hidden');
+      project1_text.classList.toggle('hidden');
+      project1_button.textContent = isHidden ? 'Hide Details' : 'Show Details';
+    });
